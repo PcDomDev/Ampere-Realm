7f53d8eb9ecc04c11198ddcecf58f56170717af3
PcDomDev <pcdomde@gmail.com>
2026-09-08 22:07:38 +0200

    fixed animation

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from engine.game_object import GameObject
 from scenes.game_scene import build_game_scene
 
 def main():
-    engine = Engine(width=1920, height=1080, title="My Custom 2D Engine", fps=60)
+    engine = Engine(width=1920, height=1080, title="Ampere Realm", fps=60)
     scene = build_game_scene()
     engine.load_scene("game_scene", scene)
 
     engine.run()
+
+    # Добавить сетку сетку на ф3
+    # Добавить компоненит звук
+    # Переписать иирархию папок
+    # Ускорить движок
 
 if __name__ == "__main__":
     main()
--- scenes/game_scene.py
+++ scenes/game_scene.py
@@ -1,6 +1,7 @@
 import json
 import pygame
 
+from engine.components import transform
 from engine.scene import Scene
 from engine.game_object import GameObject
 from engine.primitives import create_rectangle
@@ -15,12 +16,21 @@ from scripts.json_reader import load_spritesheet_animations
 def build_game_scene():
     scene = Scene()
 
-    animations = load_spritesheet_animations("assets/player/player.json", "assets/player/player.png")
+    animations = load_spritesheet_animations("assets/player/right/player.json", "assets/player/right/player.png")
 
-    player = GameObject(x=100, y=100, name="Player")
+    player = GameObject(x=0, y=-100, name="Player")
     player.add_component(SpriteRenderer(z_index=1))
-    player.add_component(Rigidbody2D(gravity=900, use_gravity=True, drag=3.0))
-    player.add_component(BoxCollider2D(size=(90, 110), anchor="midbottom"))
+    player.add_component(Rigidbody2D(
+        gravity=900,
+        use_gravity=True,
+        drag=3.0))
+
+    player.add_component(BoxCollider2D(
+        size=(100, 150),
+        offset_x=15,
+        offset_y=38,
+        anchor="topleft"
+    ))
 
     player.add_component(Animator(
         animations=animations,
@@ -35,27 +45,41 @@ def build_game_scene():
         max_jumps=2,
         anim_map={
             "idle": "idle",
-            "run": "walk",
-            "jump": "jump",
-            "fall": "jump"
+            "walk_left": "walk",
+            "walk_right": "walk",
+            "jump": "jump"
         }
     ))
 
     scene.add_game_object(player)
 
-    ground = create_rectangle(
-        x=-500, y=500,
-        width=2000, height=100,
-        color=(80, 80, 80),
-        add_collider=True,
-        add_rigidbody=False
-    )
-    scene.add_game_object(ground)
 
-    scene.add_game_object(player)
     camera_go = GameObject(name="Main Camera")
-    camera_comp = camera_go.add_component(Camera(target=player, follow_speed=6.0, smooth_follow=True))
+    camera_comp = camera_go.add_component(Camera(
+        target=player,
+        follow_speed=6.0,
+        smooth_follow=True
+    ))
+
     scene.add_game_object(camera_go)
     scene.set_active_camera(camera_comp)
 
+    scene.add_game_object(crete_box(9, 0, 0))
+    scene.add_game_object(crete_box(10, 128, 0))
+    scene.add_game_object(crete_box(11, 256, 0))
+
+
     return scene
+
+
+def crete_box(number, x, y, z_index=0, sprite=pygame.image.load("assets/separately/Block.png"), size=(128, 128), offset_x=24, offset_y=48, anchor="topleft"):
+    block = GameObject(x=x, y=y, name=str(number))
+    block.add_component(SpriteRenderer(z_index=z_index, sprite=sprite))
+    block.add_component(BoxCollider2D(
+        size=size,
+        offset_x=offset_x,
+        offset_y=offset_y,
+        anchor=anchor
+    ))
+
+    return block

144730dfc053a3d5434b3eda38dd09e198d4e31f
GeorgiiYevtushenko <pcdomdev@gmail.com>
2026-09-22 15:35:07 +0200

    fixed engine bags

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from engine.core.app import Engine
 from engine.core.scene_manager import SceneManager
+from scenes.blocks_test import BlocksTestScene
 
 
 def main():
-    engine = Engine(width=1920, height=1080, title="Ampere Realm", fps=144, fixed_fps=60)
+    engine = Engine(width=1920, height=1080, title="Ampere Realm", fps=144)
 
-    SceneManager.change_scene()
+    SceneManager.change_scene(BlocksTestScene)
 
     engine.run()
 
